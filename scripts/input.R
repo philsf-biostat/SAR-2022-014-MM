83c2d1072fa4f8cf3d728993a3eba3e8af0d8ffa
Felipe Figueiredo <philsf79@gmail.com>
2022-05-16 00:24:55 -0300

    var labels

--- scripts/input.R
+++ scripts/input.R
@@ -56,8 +56,33 @@ data.raw <- data.raw %>%
 
 data.raw <- data.raw %>%
   set_variable_labels(
-    # group = "Study group",
-    # outcome = "Study outcome",
+    sexo = "Sexo",
+    idade = "Idade (anos)",
+    imc = "IMC (kg/m²)",
+    volemia = "Volemia (L)",
+    hhs_pre = "HHS",
+    hhs_6s = "HHS",
+    lado_1 = "Primeiro lado operado",
+    cha_cir = "CHA",
+    cha_24 = "CHA",
+    cha_48 = "CHA",
+    hb_pre = "Hemoglobina pré-op",
+    hb_24 = "Hemoglobina 24h",
+    hb_48 = "Hemoglobina 48h",
+    ht_pre = "Hematócritos pré-op",
+    ht_24 = "Hematócritos 24h",
+    ht_48 = "Hematócritos 48h",
+    eva_pre = "EVA pré-op",
+    eva_24 = "EVA 24h",
+    eva_48 = "EVA 48h",
+    perda_hb_24 = "Perda de hemoglobina 24h",
+    perda_hb_48 = "Perda de hemoglobina 48h",
+    perda_sang_24 = "Perda sanguínea 24h",
+    perda_sang_48 = "Perda sanguínea 48h",
+    vol_tr = "Volume transfundido",
+    vol_24 = "Volume infundido 24h",
+    vol_48 = "Volume infundido 48h",
+    tempo_alta = "Tempo até alta (dias)",
   )
 
 # analytical dataset ------------------------------------------------------
