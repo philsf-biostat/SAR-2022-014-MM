f7a0102696e6019163b361492892a958834ebe17
Felipe Figueiredo <philsf79@gmail.com>
2022-05-16 02:01:52 -0300

    protótipo teste t pareado

--- scripts/inference.R
+++ scripts/inference.R
@@ -3,6 +3,68 @@
 
 # tables ------------------------------------------------------------------
 
+t_24 <- analytical %>%
+  # select(id, c(ends_with("pre"), hb_24, ht_24, eva_24, cha_24)) %>%
+  # select(id, hb_pre, hb_24, ht_pre, ht_24, eva_pre, eva_24) %>%
+  select(-ends_with("48"), -starts_with(c("perda", "vol", "cha", "hhs"))) %>%
+  pivot_longer(
+    -c(id),
+    ) %>%
+  separate(name, into = c("name", "time")) %>%
+  mutate(
+    time = ifelse(time == "pre", time, "pos"),
+  ) %>%
+  pivot_wider() %>%
+  mutate(
+    time = factor(time, levels = c("pre", "pos")),
+  ) %>%
+  tbl_summary(
+    include = -id,
+    by = time,
+    label = c(
+      hb = attr(analytical$hb_24, "label"),
+      ht = attr(analytical$ht_24, "label"),
+      eva = attr(analytical$eva_24, "label")
+    ),
+  ) %>%
+  add_difference(
+    # test = list(all_continuous() ~ "paired.t.test", all_categorical() ~ "mcnemar.test"),
+    test = all_continuous() ~ "paired.t.test",
+    group = id,
+  )
+
+t_48 <- analytical %>%
+  select(-ends_with("24"), -starts_with(c("perda", "vol", "cha"))) %>%
+  pivot_longer(
+    -c(id),
+  ) %>%
+  separate(name, into = c("name", "time")) %>%
+  mutate(
+    time = ifelse(time == "pre", time, "pos"),
+  ) %>%
+  pivot_wider() %>%
+  mutate(
+    time = factor(time, levels = c("pre", "pos")),
+  ) %>%
+  tbl_summary(
+    include = -id,
+    by = time,
+    label = c(
+      hb = attr(analytical$hb_48, "label"),
+      ht = attr(analytical$ht_48, "label"),
+      eva = attr(analytical$eva_48, "label"),
+      hhs = attr(analytical$hhs_pre, "label")
+    ),
+    type = list(eva ~ "continuous"),
+    ) %>%
+  add_difference(
+    # test = list(all_continuous() ~ "paired.t.test", all_categorical() ~ "mcnemar.test"),
+    test = all_continuous() ~ "paired.t.test",
+    group = id,
+  )
+
+tab_inf <- tbl_stack(list(t_24, t_48))
+
 # template p-value table
 # tab_inf <- analytical %>%
 #   tbl_summary(
